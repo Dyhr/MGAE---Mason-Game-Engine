dfdefd8e6efd090cbc3423be4e341371b7c77476
Carolina Bermejo Pascual <berm@PC608945.itu.local>
2016-10-06 15:45:39 +0200

    exercise 6_1_3 hand in

--- Engine/exercisew6-1/exercise 3/src/Box2DTest.cpp
+++ Engine/exercisew6-1/exercise 3/src/Box2DTest.cpp
@@ -9,26 +9,96 @@
 #include <glm/gtx/closest_point.hpp>
 #include <time.h>       /* time */
 
+#include "SREDebugDraw.h"
+
 
 using namespace glm;
 using namespace SRE;
 
 b2World* Box2DTest::world;
 
+void Box2DTest::initWorld(b2World* world) {
+	b2PolygonShape pillar1bottom;
+	b2PolygonShape pillar1top;
+	b2PolygonShape pillar2bottom;
+	b2PolygonShape pillar2top;
+	b2PolygonShape pillar3;
+	b2PolygonShape pillar4top;
+	b2PolygonShape pillar4bottom;
+
+	b2EdgeShape floor;
+	floor.Set(b2Vec2(0, 20), b2Vec2(640, 20));
+
+	b2EdgeShape ceiling;
+	ceiling.Set(b2Vec2(0, 460), b2Vec2(640, 460));
+
+
+	pillar1bottom.SetAsBox(20, 80, b2Vec2(240, 100), 0.0f);
+	pillar1top.SetAsBox(20, 100, b2Vec2(240, 360), 0.0f);
+	pillar2bottom.SetAsBox(20, 120, b2Vec2(360, 140), 0.0f);
+	pillar2top.SetAsBox(20, 60, b2Vec2(360, 400), 0.0f);
+	pillar3.SetAsBox(20, 180, b2Vec2(480, 200), 0.0f);
+	pillar4bottom.SetAsBox(20, 100, b2Vec2(600, 120), 0.0f);
+	pillar4top.SetAsBox(20, 80, b2Vec2(600, 380), 0.0f);
+
+
+	b2BodyDef birdbd;
+	birdbd.position.Set(30, 40);
+	birdbd.type = b2_dynamicBody;
+	birdbd.fixedRotation = true;
+	birdbody = world->CreateBody(&birdbd);
+
+	b2CircleShape bird;
+	bird.m_radius = 10;
+	b2FixtureDef fd;
+	fd.shape = &bird;
+	fd.density = 20.0f;
+	fd.friction = 1.0f;
+
+	birdbody->CreateFixture(&fd);
+
+
+	b2BodyDef bd;
+	b2Body* ground = world->CreateBody(&bd);//ground includes all the elements in the game world
+
+	ground->CreateFixture(&pillar1bottom, 0.0f);
+	ground->CreateFixture(&pillar1top, 0.0f);
+	ground->CreateFixture(&pillar2bottom, 0.0f);
+	ground->CreateFixture(&pillar2top, 0.0f);
+	ground->CreateFixture(&pillar3, 0.0f);
+	ground->CreateFixture(&pillar4top, 0.0f);
+	ground->CreateFixture(&pillar4bottom, 0.0f);
+	ground->CreateFixture(&floor, 0.0f);
+	ground->CreateFixture(&ceiling, 0.0f);
+}
+
+void Box2DTest::update() {
+	if (reset) {
+		reset = false;
+		birdbody->SetTransform(b2Vec2(30, 40), 0);
+		hasLeftTheGround = 0;
+		birdbody->SetLinearVelocity(b2Vec2(0, 0));
+	}
+
+	if (hasLeftTheGround) {
+		b2Vec2 v = birdbody->GetLinearVelocity();
+		v.x = 50;
+		birdbody->SetLinearVelocity(v);
+	}
+}
+
 Box2DTest::Box2DTest(int width, int height) {
     this->width = width;
     this->height = height;
     
-    //TODO: Initialize your world from here!
-    Box2DTest::world = new b2World(b2Vec2(0,-9.89f));
+    Box2DTest::world = new b2World(b2Vec2(0,-500));
+	initWorld(world);
  
+    world->SetContactListener(this);
     
-    //TODO: Implement make  Box2DTest extend b2ContactListener, to allow for collision handling!
-    //world->SetContactListener(this);
-    
-    
-    //TODO: Set your debug Draw instance here!
-    //world->SetDebugDraw(b2Draw *debugDraw);
+	auto debugDraw = new SREDebugDraw();
+    world->SetDebugDraw(debugDraw);
+	debugDraw->SetFlags(b2Draw::e_shapeBit);
 }
 
 Box2DTest::~Box2DTest() {
@@ -58,6 +128,8 @@ void Box2DTest::startGameLoop() {
         
         //Update physics
         world->Step(1/60.f, 10, 10);
+
+		update();
         
         //Debug Draw your physics
         world->DrawDebugData();
@@ -67,8 +139,11 @@ void Box2DTest::startGameLoop() {
     }
 }
 
-//TODO: implement BeginContact from the contactListener, to handle collisions
-//void BeginContact(b2Contact* contact)
+void Box2DTest::BeginContact(b2Contact* contact) {
+	if (hasLeftTheGround) {
+		reset = true;
+	}
+}
 
 void Box2DTest::HandleSDLEvents()
 {
@@ -81,8 +156,9 @@ void Box2DTest::HandleSDLEvents()
             case SDL_KEYDOWN:
                 if(event.key.keysym.sym == SDLK_SPACE)
                 {
-                    //TODO: Implement your keyboard handling logic here!
-                    std::cout << "Test" << std::endl;
+					float impulse = birdbody->GetMass() * 5000000000;
+					birdbody->ApplyForceToCenter(b2Vec2(0, impulse), true);
+					hasLeftTheGround = true;
                 }
                 break;
                 /* SDL_QUIT event (window close) */
--- Engine/exercisew6-1/exercise 3/src/Box2DTest.hpp
+++ Engine/exercisew6-1/exercise 3/src/Box2DTest.hpp
@@ -4,18 +4,27 @@
 #include "SDL.h"
 #include "Box2D/Box2D.h"
 
-class Box2DTest {
+class Box2DTest : public b2ContactListener {
 public:
     Box2DTest(int width, int height);
     virtual ~Box2DTest();
     void startGameLoop();
     static b2World* world;
     void HandleSDLEvents();
-    
+
+	void BeginContact(b2Contact* contact);
+
 private:
+
+	void initWorld(b2World* world);
+	void update();
 
     int width;
     int height;
     int quit;
+
+	b2Body* birdbody;
+	bool hasLeftTheGround = false;
+	bool reset = false;
 };
 
