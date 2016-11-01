001caee2bebf3406eadac5b2f88c82fb4d9840fa
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2016-11-01 11:09:44 +0100

    Actually removed files

--- Engine/src/Collider.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Component.hpp"
-#include "Physics.hpp"
-
-class Collider : public Component {
-public:
-protected:
-	Collider(GameObject* gameObject) : Component(gameObject) { }
-};
