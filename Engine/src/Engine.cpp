a0bd036a528467dd299bd9318c6b4f3491dc69a1
Carolina Bermejo Pascual <berm@itu.dk>
2016-11-21 17:18:41 +0100

    particles with texture

--- Engine/src/Engine.cpp
+++ Engine/src/Engine.cpp
@@ -218,7 +218,9 @@ void Engine::loadScene(std::string path)
 }
 
 void Engine::update(float deltaTimeSec) {
+	auto tex = SRE::Texture::createFromFile("data/dice.PNG", false);
 	sre->clearScreen({ 0,0,1,1 });
+	numberSprites = 0;
 
 	// fetch input
 	SDL_Event event;
@@ -273,9 +275,11 @@ void Engine::update(float deltaTimeSec) {
 				particleEmitter->update();
 			}
 		}
-		ParticleEmitter::render();
+		ParticleEmitter::render(tex);
+		delete tex; //makeshift to show how to 
 	}
-
+	if (InputManager::getInstance()->toggleGUI)
+		DebugUI();
 	sre->swapWindow();
 }
 
