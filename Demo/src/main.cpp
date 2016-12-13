7398b538696cbec0a6b2398f4a38fac484d483b5
Carolina Bermejo Pascual <berm@itu.dk>
2016-12-13 13:36:23 +0100

    did I merge right?
    hopefully

--- Demo/src/main.cpp
+++ Demo/src/main.cpp
@@ -14,7 +14,7 @@ using namespace Mason;
 Engine engine;
 
 void loadScene(int index) {
-<<<<<<< HEAD
+
 	engine.loadScene("data/demoCollisions.json");
 	//marios
 	
@@ -89,65 +89,8 @@ void loadScene(int index) {
 	auto t = engine.scene->getGameObject(17)->addComponent<BoxCollider2D>();
 	engine.scene->getGameObject(17)->addComponent<PlayerController>();
 	t->setSize(55, 45);
-=======
-	//engine.loadScene("data/demoCollisions.json");
-	////marios
-	//engine.scene->getGameObject(9)->addComponent<PlayerController>();
-	//engine.scene->getGameObject(9)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_dynamicBody);
-	//auto m = engine.scene->getGameObject(9)->addComponent<CircleCollider2D>();
-	//m->setSize(18);
 
 
-	//engine.scene->getGameObject(10)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_dynamicBody);
-	//auto m1 = engine.scene->getGameObject(10)->addComponent<CircleCollider2D>();
-	//m1->setSize(18);
-
-
-	//engine.scene->getGameObject(2)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_dynamicBody);
-	//auto m2 = engine.scene->getGameObject(2)->addComponent<CircleCollider2D>();
-	//m2->setSize(18);
-
-	//engine.scene->getGameObject(3)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_dynamicBody);
-	//auto m3 = engine.scene->getGameObject(3)->addComponent<CircleCollider2D>();
-	//m3->setSize(18);
-
-	//engine.scene->getGameObject(4)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_dynamicBody);
-	//auto m4 = engine.scene->getGameObject(4)->addComponent<CircleCollider2D>();
-	//m4->setSize(18);
-
-	////bricks
-	//engine.scene->getGameObject(3)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_kinematicBody);
-	//engine.scene->getGameObject(3)->addComponent<DangerousCollisions>();
-	//auto b = engine.scene->getGameObject(3)->addComponent<BoxCollider2D>();
-	//b->setSize(35, 35);
-
-	//engine.scene->getGameObject(4)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_kinematicBody);
-	//engine.scene->getGameObject(4)->addComponent<DangerousCollisions>();
-	//auto b2 = engine.scene->getGameObject(4)->addComponent<BoxCollider2D>();
-	//b2->setSize(35, 40);
-
-	//engine.scene->getGameObject(5)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_kinematicBody);
-	//engine.scene->getGameObject(5)->addComponent<DangerousCollisions>();
-	//auto b3 = engine.scene->getGameObject(5)->addComponent<BoxCollider2D>();
-	//b3->setSize(35, 40);
-
-	//engine.scene->getGameObject(6)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_kinematicBody);
-	//engine.scene->getGameObject(6)->addComponent<DangerousCollisions>();
-	//auto b4 = engine.scene->getGameObject(6)->addComponent<BoxCollider2D>();
-	//b4->setSize(35, 40);
-
-	//engine.scene->getGameObject(7)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_kinematicBody);
-	//engine.scene->getGameObject(7)->addComponent<DangerousCollisions>();
-	//auto b5 = engine.scene->getGameObject(7)->addComponent<BoxCollider2D>();
-	//b5->setSize(35, 40);
-
-	////greentube
-	//engine.scene->getGameObject(8)->addComponent<PhysicsBody2D>()->body->SetType(b2BodyType::b2_kinematicBody);
-	//auto b6 = engine.scene->getGameObject(8)->addComponent<BoxCollider2D>();
-	//b6->setSize(55, 45);
-	engine.loadScene("data/demoParticles.json");
-	engine.scene->getGameObject(0)->addComponent<PlayerController>();
->>>>>>> origin/master
 }
 
 
