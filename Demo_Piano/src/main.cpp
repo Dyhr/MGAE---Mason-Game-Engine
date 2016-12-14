476687046d0ebe592316823853fb9f9500464de2
Bjørn <bjol@itu.dk>
2016-12-14 10:18:14 +0100

    Separate projects for particles demo and piano demo

---
+++ Demo_Piano/src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <Mason/Engine.hpp>
+#include "Mason/InputManager.h"
+#include "PlayerController.hpp"
+
+using namespace glm;
+using namespace Mason;
+
+Engine engine;
+
+int main(int argc, char** argv) {
+	
+	engine.loadScene("data/demoParticles.json");
+	engine.scene->getGameObject(0)->addComponent<PlayerController>();
+	engine.start();
+
+	return 0;
+}
