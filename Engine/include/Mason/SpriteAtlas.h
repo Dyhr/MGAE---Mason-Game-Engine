c098e83971a90a8ba489bce233863b54ea9b52c6
Carolina Bermejo Pascual <berm@itu.dk>
2016-12-14 18:33:54 +0100

    sprites commented
    for doxygen

--- Engine/include/Mason/SpriteAtlas.h
+++ Engine/include/Mason/SpriteAtlas.h
@@ -5,6 +5,25 @@
 #include <memory>
 
 namespace Mason {
+	/** @class SpriteAtlas
+	* @brief Manage different sprites packed in a single texture (spritesheet).
+	*
+	* The information about the location and pivot point of each sprite is stored in a JSON file
+	*
+	* @fn SpriteAtlas(std::string atlasJsonDataDirectory, std::string atlasJsonDataFile)
+	* @brief Constructor to work with a specific SpriteAtlas
+	* @param atlasJsonDataDirectory path to the directory where the JSON file with the info of the sprites is
+	* @param atlasJsonDataFile name of the JSON file with the info of the sprites
+	*
+	* @fn getSprite(std::string name)
+	* @brief get a specific sprite from the SpriteAtlas
+	* @param name unique name that identifies the @see Sprite
+	* @return a pointer to the @see Sprite
+	*
+	*  @file MarioPacked.json
+	*  This is the JSON file for the SpriteAtlas used for the physics demo
+	*  @verbinclude MarioPacked.json
+	*/
 
 	class Sprite;
 
