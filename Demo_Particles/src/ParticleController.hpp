1fa1a3355b76e05393f1b147a499e76130ad1c1b
Bjørn <bjol@itu.dk>
2016-12-17 21:59:03 +0100

    particle demo stuff

--- Demo_Particles/src/ParticleController.hpp
+++ Demo_Particles/src/ParticleController.hpp
@@ -20,12 +20,13 @@ public:
 
 	void OnStart() override {
 		emitter = gameObject->getComponent<ParticleEmitter>();
+
 	}
  	void OnInput(SDL_Event event) override {
 		if (event.type == SDL_KEYDOWN)
 		{
-			if (event.key.keysym.sym == SDLK_n) emitter->stop();
-			if (event.key.keysym.sym == SDLK_m) emitter->start();
+			if (char(event.key.keysym.sym == strings["stop"][0])) emitter->stop();
+			if (char(event.key.keysym.sym == strings["start"][0])) emitter->start();
 		}
 	}
 };
