2b0e061c8937e81d7c8bf8b0a49e89693a8c60f5
Bjørn <bjol@itu.dk>
2016-12-18 23:35:27 +0100

    doxygen for component.hpp

--- Engine/include/Mason/Component.hpp
+++ Engine/include/Mason/Component.hpp
@@ -4,14 +4,21 @@
 namespace Mason {
 
 	class GameObject;
-
+	/** @class Component super class for all components. Has a shared pointer to its gameobject
+	*/
 	class Component {
 	public:
+		/** @brief Destructor.
+		*/
 		virtual ~Component();
+		/** @brief Returns a shared pointer to the gameobject of the component.
+		*/
 		std::shared_ptr<GameObject> getGameObject();
 	protected:
+		/** @brief Constructor.
+		*/
 		Component(std::shared_ptr<GameObject> gameObject);
-		std::shared_ptr<GameObject> gameObject;
+		std::shared_ptr<GameObject> gameObject; ///< shared pointer to gameobject
 
 		friend class GameObject;
 	};
